7459ef3d1951ff274ca1e8ac61b412a95914de5c
Danish Khan <danishkhan@zapbuild.com>
2020-08-11 10:45:15 +0530

    Initial Commit

---
+++ FirebaseLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by Danish Khan on 11/08/20.
+//  Copyright © 2020 Danish Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
